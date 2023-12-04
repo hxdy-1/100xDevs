f980931a9977b0f65bcf78b93e17240eaec492aa
hady <hadimomin02@gmail.com>
2023-12-04 16:30:06 +0530

    done easy problems

--- 1-Basics of JS/assignments-master/01-js/easy/anagram.js
+++ 1-Basics of JS/assignments-master/01-js/easy/anagram.js
@@ -5,7 +5,41 @@
 */
 
 function isAnagram(str1, str2) {
+	if (str1.length !== str2.length) {
+		return false;
+	}
 
+	const normalizer = (str) => str.toLowerCase();
+
+	const charCounter = (str) => {
+		const charObj = {};
+
+		for (let char of str) {
+			charObj[char] = (charObj[char] || 0) + 1;
+		}
+
+		return charObj;
+	};
+
+	const normalizedStr1 = normalizer(str1);
+	const normalizedStr2 = normalizer(str2);
+
+	// if (normalizedStr1.length !== normalizedStr2.length) {
+	// 	return false;
+	// }
+
+	const charInNormalizedStr1 = charCounter(normalizedStr1);
+	const charInNormalizedStr2 = charCounter(normalizedStr2);
+
+	for (let char in charInNormalizedStr1) {
+		if (charInNormalizedStr1[char] !== charInNormalizedStr2[char]) {
+			return false;
+		}
+	}
+
+	return true;
 }
 
 module.exports = isAnagram;
+
+// we have to check: lengths of both strings (before or after normalization doesn't matter), occurrence of each character in both the strings.
--- 1-Basics of JS/assignments-master/01-js/easy/expenditure-analysis.js
+++ 1-Basics of JS/assignments-master/01-js/easy/expenditure-analysis.js
@@ -6,7 +6,24 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+	const categoryTotals = {};
+
+	transactions.forEach((transaction) => {
+		if (!categoryTotals[transaction.category]) {
+			categoryTotals[transaction.category] = transaction.price;
+		} else {
+			categoryTotals[transaction.category] += transaction.price;
+		}
+	});
+
+	const resultArray = Object.keys(categoryTotals).map((category) => {
+		return { category, totalSpent: categoryTotals[category] };
+	});
+
+	return resultArray;
+	// return [];
 }
 
 module.exports = calculateTotalSpentByCategory;
+
+// we have to return an array of objects, each object representing the category as key and total amount spent as value.
