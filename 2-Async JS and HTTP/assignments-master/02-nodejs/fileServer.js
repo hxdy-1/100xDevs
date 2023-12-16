828e17757d6441c4aa046ca6f23658fcced53eb4
hady <hadimomin02@gmail.com>
2023-12-16 20:57:26 +0530

    finish nodes js assignments

--- 2-Async JS and HTTP/assignments-master/02-nodejs/fileServer.js
+++ 2-Async JS and HTTP/assignments-master/02-nodejs/fileServer.js
@@ -12,10 +12,41 @@
     - For any other route not defined in the server return 404
     Testing the server - run `npm run test-fileServer` command in terminal
  */
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 
+app.get("/files", (req, res) => {
+	const directoryPath = path.join(__dirname, "./files/");
+	// const directoryPath = path.join(__dirname, "files");
+
+	fs.readdir(directoryPath, (err, files) => {
+		if (err) {
+			return res.status(500).json({ error: "Failed to retrieve files" });
+		}
+
+		res.json(files);
+	});
+});
+
+app.get("/files/:filename", (req, res) => {
+	const directoryPath = path.join(__dirname, "./files/", req.params.filename);
+	// const directoryPath = path.join(__dirname, "files", req.params.filename);
+
+	fs.readFile(directoryPath, "utf-8", (err, data) => {
+		if (err) {
+			return res.status(404).send("File not found");
+		}
+
+		res.send(data);
+	});
+});
+
+app.get("*", (req, res) => {
+	res.status(404).send("Route not found");
+});
+
+app.listen(3000);
 
 module.exports = app;
--- 2-Async JS and HTTP/assignments-master/02-nodejs/todoServer.js
+++ 2-Async JS and HTTP/assignments-master/02-nodejs/todoServer.js
@@ -39,11 +39,69 @@
 
   Testing the server - run `npm run test-todoServer` command in terminal
  */
-  const express = require('express');
-  const bodyParser = require('body-parser');
-  
-  const app = express();
-  
-  app.use(bodyParser.json());
-  
-  module.exports = app;
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+let todos = [];
+
+app.get("/todos", (req, res) => {
+	res.json(todos);
+});
+
+app.get("/todos/:id", (req, res) => {
+	const requiredTodo = todos.find((todo) => todo.id === +req.params.id);
+
+	if (!requiredTodo) {
+		res.status(404).send();
+	} else {
+		res.json(requiredTodo);
+	}
+});
+
+app.post("/todos", (req, res) => {
+	const newTodo = {
+		id: Math.floor(Math.random() * 1000000),
+		title: req.body.title,
+		description: req.body.description,
+	};
+	todos.push(newTodo);
+
+	res.status(201).json(newTodo);
+});
+
+app.put("todos/:id", (req, res) => {
+	const requiredTodoIndex = todos.findIndex(
+		(todo) => todo.id === +req.params.id
+	);
+
+	if (requiredTodoIndex < 0 && requiredTodoIndex >= todos.length) {
+		res.status(404).send();
+	} else {
+		todos[requiredTodoIndex].title = req.body.title;
+		todos[requiredTodoIndex].description = req.body.description;
+		res.json(todos[requiredTodoIndex]);
+	}
+});
+
+app.delete("/todos/:id", (req, res) => {
+	const requiredTodoIndex = todos.findIndex(
+		(todo) => todo.id === +req.params.id
+	);
+
+	if (requiredTodoIndex < 0 && requiredTodoIndex >= todos.length) {
+		res.status(404).send();
+	} else {
+		todos.splice(requiredTodoIndex, 1);
+		res.status(200).send();
+	}
+});
+
+app.get("*", (req, res) => {
+	res.status(404).send("Route not found");
+});
+
+module.exports = app;
