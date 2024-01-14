fbe4d2d0af34fe7fdc4b2ac531547f73c0faafbe
hady <hadimomin02@gmail.com>
2024-01-14 20:47:26 +0530

    end recoil intro

--- 7-Routing and Context-API/live-class/context-and-recoil/src/App.jsx
+++ 7-Routing and Context-API/live-class/context-and-recoil/src/App.jsx
@@ -1,40 +1,58 @@
 import { useContext, useState } from "react";
 import { CountContext } from "./Context";
+import {
+	RecoilRoot,
+	useRecoilState,
+	useRecoilValue,
+	useSetRecoilState,
+} from "recoil";
+import { countAtom, isEvenSelector } from "../store/atoms/Count";
 
 function App() {
-	const [count, setCount] = useState(0);
+	// const [count, setCount] = useState(0);
 	// wrap anyone that wants to use the teleported value inside a provider
 	// recoil, redux, Themes in mUI
 	return (
 		<div>
-			<CountContext.Provider value={count}>
-				<Count setCount={setCount} />
-			</CountContext.Provider>
+			{/* <CountContext.Provider value={count}> */}
+			<RecoilRoot>
+				<Count />
+			</RecoilRoot>
+			{/* </CountContext.Provider> */}
 		</div>
 	);
 }
 
-function Count({ setCount }) {
+// function Count({ setCount }) {
+function Count() {
+	console.log("Count comp rendered");
+	const count = useRecoilValue(countAtom);
+	const isEven = useRecoilValue(isEvenSelector);
 	return (
 		<div>
 			<CountRenderer />
-			<Buttons setCount={setCount} />
+			{/* <Buttons setCount={setCount} /> */}
+			<Buttons />
+			{isEven ? "It is even" : ""}
 		</div>
 	);
 }
 
 function CountRenderer() {
-	const count = useContext(CountContext);
+	// const count = useContext(CountContext);
+	const count = useRecoilValue(countAtom);
 	return <div>{count}</div>;
 }
 
-function Buttons({ setCount }) {
-	const count = useContext(CountContext);
+// function Buttons({ setCount }) {
+function Buttons() {
+	// const count = useContext(CountContext);
+	const setCount = useSetRecoilState(countAtom);
 	return (
 		<div>
 			<button
 				onClick={() => {
-					setCount(count + 1);
+					setCount((prev) => prev + 1);
 				}}
 			>
 				Increase
@@ -42,7 +60,7 @@ function Buttons({ setCount }) {
 
 			<button
 				onClick={() => {
-					setCount(count - 1);
+					setCount((prev) => prev - 1);
 				}}
 			>
 				Decrease
--- 7-Routing and Context-API/live-class/context-and-recoil/store/atoms/Count.jsx
+++ 7-Routing and Context-API/live-class/context-and-recoil/store/atoms/Count.jsx
@@ -1,6 +1,14 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export const countAtom = atom({
 	key: "countAtom",
 	default: 0,
+});
+
+export const isEvenSelector = selector({
+	key: "isEvenSelector",
+	get: ({ get }) => {
+		const isEven = get(countAtom);
+		return isEven % 2 == 0 && isEven != 0;
+	},
 });
