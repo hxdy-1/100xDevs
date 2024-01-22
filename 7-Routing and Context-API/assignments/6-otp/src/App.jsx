60b087295b5db66697e4de47be71a861579d733a
hady <hadimomin02@gmail.com>
2024-01-22 21:47:37 +0530

    finish 6-otp assignment

---
+++ 7-Routing and Context-API/assignments/6-otp/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import PhoneOtpForm from "./components/PhoneLogin";
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Login with Phone</h1>
+			<PhoneOtpForm />
+		</div>
+	);
+}
+
+export default App;
